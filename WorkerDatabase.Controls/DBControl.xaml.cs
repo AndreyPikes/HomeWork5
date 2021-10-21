205e3ab77b5a2c8134933a75ada3cdc1bcaa9233
Андрей Щукин <1929999@gmail.com>
2021-10-21 17:21:47 +0300

    HomeWork5

---
+++ WorkerDatabase.Controls/DBControl.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Workers;
+using Workers.Data;
+
+namespace WorkerDatabase.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для DBControl.xaml
+    /// </summary>
+    public partial class DBControl : UserControl
+    {
+        private Worker worker;
+        //private WorkersDatabase workersDatabase = new WorkersDatabase();
+        public DBControl()
+        {
+            InitializeComponent();
+
+            cbxDepartment.ItemsSource = WorkersDatabase.ListDepartment;            
+        }
+
+        /// <summary>
+        /// извне устанавливаем дефолтный департамент, чтобы невозможно было создать работника вообще без департамента
+        /// </summary>
+        public void SetDefaultDepartment() 
+        {
+            cbxDepartment.SelectedIndex = 0;
+        }
+
+        public void SetWorker(Worker worker)
+        {
+            this.worker = worker;
+            tbxName.Text = worker.Name;
+            tbxAge.Text = worker.Age.ToString();
+            tbxPosition.Text = worker.Position;
+            tbxComment.Text = worker.Comment;
+            chbxSecrecy.IsChecked = worker.Secrecy;
+            cbxDepartment.SelectedItem = worker.Department;
+        }
+
+        public void UpdateWorker()
+        {
+            worker.Name = tbxName.Text;            
+            if (int.TryParse(tbxAge.Text, out int age)) worker.Age = age;
+            worker.Position = tbxPosition.Text;
+            worker.Comment = tbxComment.Text;
+            worker.Secrecy = (bool)chbxSecrecy.IsChecked;
+            worker.Department = (Department)cbxDepartment.SelectedItem;
+        }
+
+        private void cbxDepartment_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            cbxDepartment.ItemsSource = null;
+            cbxDepartment.ItemsSource = WorkersDatabase.ListDepartment;
+        }
+
+        
+    }
+}
---
+++ Workers.Data/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers.Data
+{
+    public class Department : IEquatable<Department>
+    {
+        public string NameDepartment { get; set; }
+
+        public Department(string name)
+        {
+            NameDepartment = name;
+        }
+
+        public bool Equals(Department other)
+        {
+            return this.NameDepartment == other.NameDepartment;
+        }
+    }
+}
